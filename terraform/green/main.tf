48228420e8bee957f1cc7c4aced97fdf5e7dea99
Toru Makabe <tomakabe@microsoft.com>
2022-05-27 14:09:02 +0900

    upgrade: tf, tf provider and helm charts

--- terraform/green/main.tf
+++ terraform/green/main.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.2.1"
   # Choose the backend according to your requirements
   # backend "remote" {}
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.7.0"
+      version = "~> 3.8.0"
     }
 
     kubernetes = {
