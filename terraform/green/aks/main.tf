857914466cde0cdaebfbe39470af02cb9eef7033
Toru Makabe <993850+torumakabe@users.noreply.github.com>
2022-09-26 10:05:23 +0900

    bump: tools, provider and charts

--- terraform/green/aks/main.tf
+++ terraform/green/aks/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.2.9"
+  required_version = "~> 1.3.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.22.0"
+      version = "~> 3.24.0"
     }
   }
 }
